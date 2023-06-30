de55d5a89ae6d53f1782e6b9c6d1f44a63a5177a
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-30 08:19:23 +0100

    restaurants component done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CitiesComponent } from './cities/cities.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CitiesComponent
+    CitiesComponent,
+    RestaurantComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
