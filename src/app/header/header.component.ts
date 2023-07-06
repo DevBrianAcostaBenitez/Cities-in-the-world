c7999f3c9548501102f39712f911f6682e5816f9
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-07-06 20:43:52 +0100

    hover in header component in home, about and contact

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  showHomeMessage: boolean = false;
+  showAboutMessage: boolean = false;
+  showContactMessage: boolean = false;
 
+  onMouseEnterHome() {
+    this.showHomeMessage = true;
+  }
+
+  onMouseLeaveHome() {
+    this.showHomeMessage = false;
+  }
+
+  onMouseEnterAbout() {
+    this.showAboutMessage = true;
+  }
+
+  onMouseLeaveAbout() {
+    this.showAboutMessage = false;
+  }
+
+  onMouseEnterContact() {
+    this.showContactMessage = true;
+  }
+
+  onMouseLeaveContact() {
+    this.showContactMessage = false;
+  }
 }
